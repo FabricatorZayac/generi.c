b2bf8a0a064f311a5a9345d398f7ad326e18a778
Eugene Fedchuk <fed4chuck@gmail.com>
2023-03-03 06:26:13 +0200

    added a stupid crutch because macros are broken

--- include/result.h
+++ include/result.h
@@ -14,4 +14,15 @@ enum Result { Ok, Err };
           of(Ok __VA_OPT__(, __VA_ARGS__)), \
           of(Err, Error e) return (Err(ret, e)))
 
+// NOTE: this is a crutch. When I nest macros too deeply shit just seems to break
+// The problem is most likely to do with how I implement `match()`
+// Either that or the problem is with my scuffed FOREACH macro
+#define _TRY(ret, expr, ...)                    \
+    if (expr.switcher == Ok) {                  \
+        __VA_OPT__(__VA_ARGS__ = expr.body.Ok); \
+    } else if (expr.switcher == Err) {          \
+        Error e = expr.body.Err;                \
+        return (Err(ret, e));                   \
+    }
+
 #endif  // RESULT_H_
